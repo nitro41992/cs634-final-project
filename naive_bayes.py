37ec731aa4f964b0dfd55a9b49eec0e1f6b062d8
nitro41992 <nick.k@iton.net>
2019-11-19 19:13:58 -0500

    Task: converted data into features and labels

--- naive_bayes.py
+++ naive_bayes.py
@@ -6,3 +6,17 @@ def get_labels(filename):
         file = csv.reader(f)
         temp = list(file)
     return temp
+
+
+def seperate_features_and_labels(file):
+    features = []
+    labels = []
+    for row in file:
+        features.append(row[1:])
+        labels.append(row[-1])
+
+    return(features, labels)
+
+
+data = get_labels('breast-cancer-wisconsin.data')
+features, labels = seperate_features_and_labels(data)
