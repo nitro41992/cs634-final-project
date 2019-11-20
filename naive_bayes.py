b7c9310d6b691b701d37fe3b51932d8526c8911e
nitro41992 <nick.k@iton.net>
2019-11-19 20:00:07 -0500

    Task: Created Predict function using NB classifier

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,4 +1,10 @@
 import csv
+import numpy as np
+from sklearn import preprocessing
+from sklearn.naive_bayes import GaussianNB
+
+le = preprocessing.LabelEncoder()
+model = GaussianNB()
 
 
 def get_labels(filename):
@@ -7,16 +13,23 @@ def get_labels(filename):
         temp = list(file)
     return temp
 
-
 def seperate_features_and_labels(file):
     features = []
     labels = []
+
     for row in file:
-        features.append(row[1:])
+        row = [float(i.replace('?','0')) for i in row]
+        features.append(tuple(row[1:]))
         labels.append(row[-1])
 
-    return(features, labels)
+    labels_encoded = le.fit_transform(labels)
+    return(features, labels_encoded)
 
 
 data = get_labels('breast-cancer-wisconsin.data')
 features, labels = seperate_features_and_labels(data)
+model.fit(features, labels)
+
+predicted = model.predict([[2, 1, 1, 1, 2, 1, 1, 1, 10, 3]])
+
+print("Predicted Value:", predicted)
