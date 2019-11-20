ad427facf4c93fd626649b30844812ba6575ac0f
nitro41992 <nick.k@iton.net>
2019-11-19 20:42:37 -0500

    Task: Performing 10-fold cross validation

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,10 +1,14 @@
 import csv
 from sklearn import preprocessing
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
 from sklearn.naive_bayes import GaussianNB
+import numpy as np
 
 le = preprocessing.LabelEncoder()
 model = GaussianNB()
-
+scaler = MinMaxScaler(feature_range=(0, 1))
 
 def get_labels(filename):
     with open(filename, "rt", encoding='utf8') as f:
@@ -15,20 +19,34 @@ def get_labels(filename):
 def seperate_features_and_labels(file):
     features = []
     labels = []
-
     for row in file:
         row = [float(i.replace('?','0')) for i in row]
         features.append(tuple(row[1:]))
         labels.append(row[-1])
 
     labels_encoded = le.fit_transform(labels)
+    features = scaler.fit_transform(features)
+
     return(features, labels_encoded)
 
 
 data = get_labels('breast-cancer-wisconsin.data')
 features, labels = seperate_features_and_labels(data)
-model.fit(features, labels)
+# model.fit(features, labels)
 
-predicted = model.predict([[2, 1, 1, 1, 2, 1, 1, 1, 10, 3]])
+scores = []
+cv = KFold(n_splits=10, random_state=42, shuffle=False)
+for train_index, test_index in cv.split(features):
+    # print("Train Index: ", train_index, "\n")
+    # print("Test Index: ", test_index, "\n")
 
-print("Predicted Value:", predicted)
+    x_train, x_test, y_train, y_test = features[train_index], features[test_index], labels[train_index], labels[test_index]
+    model.fit(x_train, y_train)
+    scores.append(model.score(x_test, y_test))
+
+print(np.mean(scores))
+
+print(cross_val_score(model, features, labels, cv=10))
+
+# predicted = model.predict([[2, 1, 1, 1, 2, 1, 1, 1, 10, 3]])
+# print("Predicted Value:", predicted)
