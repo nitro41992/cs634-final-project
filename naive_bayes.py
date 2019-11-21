a64987085e959e413733512253909df6381fa6d5
nitro41992 <nick.k@iton.net>
2019-11-20 21:10:37 -0500

    Fix: Refactoring to fix  random characters

--- naive_bayes.py
+++ naive_bayes.py
@@ -10,29 +10,20 @@ le = preprocessing.LabelEncoder()
 model = GaussianNB()
 scaler = MinMaxScaler(feature_range=(0, 1))
 
+
 def get_labels(filename):
     with open(filename, "rt", encoding='utf8') as f:
         file = csv.reader(f)
         temp = list(file)
     return temp
 
+
 def seperate_features_and_labels(file):
     features = []
     labels = []
     for row in file:
-       for i in range(len(row)):
-            if row[i] == 'M':
-                row[i] = 1
-            elif row[i] == 'B':
-                row[i] = 0
-            elif row[i] == 'R':
-                row[i] = 1
-            elif row[i] == 'N':
-                row[i] = 0
-            elif row[i] == '?':
-                row[i] = 0
-       features.append(tuple(row[1:]))
-       labels.append(row[-1])
+        features.append(tuple(le.fit_transform(row[1:])))
+        labels.append(row[1])
 
     labels_encoded = le.fit_transform(labels)
     features = scaler.fit_transform(features)
@@ -42,18 +33,17 @@ def seperate_features_and_labels(file):
 
 data = get_labels('wpbc.data')
 features, labels = seperate_features_and_labels(data)
-# model.fit(features, labels)
+print('Features: ', features)
+print('Labels: ', labels)
 
 scores = []
 splits = 10
 cv = KFold(n_splits=splits, random_state=42, shuffle=False)
 for train_index, test_index in cv.split(features):
-    # print("Train Index: ", train_index, "\n")
-    # print("Test Index: ", test_index, "\n")
 
-    x_train, x_test, y_train, y_test = features[train_index], features[test_index], labels[train_index], labels[test_index]
+    x_train, x_test, y_train, y_test = features[train_index], features[
+        test_index], labels[train_index], labels[test_index]
     model.fit(x_train, y_train)
     scores.append(model.score(x_test, y_test))
 
 print(np.mean(scores))
-
